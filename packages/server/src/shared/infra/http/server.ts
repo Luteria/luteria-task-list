5b4169178606a40f169222c8e76e145fa82edb64
Agleylson Melo <agleylsonmelo@outlook.com>
2020-08-13 12:09:50 -0300

    style: Prettier, Eslint

--- packages/server/src/shared/infra/http/server.ts
+++ packages/server/src/shared/infra/http/server.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+<<<<<<< HEAD
+import "@shared/infra/typeorm";
+
+app.listen(3333, () => {
+  console.log("Server on");
+});
+=======
 app.listen(3333);
+>>>>>>> 55c935a13c98d2c2befbcb5941e8ec5b2c6bf908
